91cc5db1b10a6fb88fc951373ffcc5ca0f7d307e
13574798@qq.com <13574798@qq.com>
2024-03-16 21:48:36 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import BBB from './components/Greet'
+import Welcome from "./components/Welcome";
+import Greet from "./components/Greet";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
+import FunctionClick from "./components/Function";
+import ParentComponent from "./components/ParentComponent";
+import {Component} from "react";
 
+import NameList from "./components/NameList";
+import StyleSheet from "./components/StyleSheet";
+import Inline from "./components/Inline";
+import Form from "./components/Form";
+import Lifecycle from "./components/Lifecycle";
+import RefsDemo from "./components/RefsDemo";
+import FrParent from "./components/FRParent";
+import PortalDom from "./components/PortalDom";
+import Hero from "./components/Hero";
+import ErrorBoundary from "./components/ErrorBoundary";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ErrorBoundary>
+        <Hero heroName="sunshine1"/>
+        </ErrorBoundary>
+
+
+        <ErrorBoundary>
+        <Hero heroName="sunshine2"/>
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+        <Hero heroName="joker"/>
+        </ErrorBoundary>
+        {/*<PortalDom/>*/}
+        {/*<FrParent/>*/}
+        {/*<RefsDemo/>*/}
+        {/*<Lifecycle/>*/}
+        {/*<Form/>*/}
+        {/*<Inline/>*/}
+        {/*{<StyleSheet primary={true}/>}*/}
+    {/*<ParentComponent/>*/}
+      {/*<Greet name="111"/>*/}
+      {/*  <Greet name="22"/>*/}
+      {/*  <Greet name="333"/>*/}
+      {/*<Welcome name="sunshine"/>*/}
+      {/*  <Message/>*/}
+      {/*  <Counter/>*/}
     </div>
   );
 }
+
 
 export default App;
